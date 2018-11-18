98792984ab70b071697a82b6c3ffbe3943635fd0
[harry borgenvik] <[harryb180497@yahoo.com]>
2018-11-18 14:33:29 +0000

    Movir Detail Page Implementation
    added in movie description page

--- SE_Artefact/main.js
+++ SE_Artefact/main.js
@@ -30,3 +30,53 @@ function getMovies(searchText) {
         console.log(err);
     });
 }
+
+function movieSelector(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.OMDbapi.com/?i=' + movieId + '&apikey=b309e8b6')
+    .then((response) => {
+        console.log(response);
+        let movie = response.data;
+
+        let output = `
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="${movie.Poster} class="thumbnail">
+                </div>
+                <div class="col-md-8">
+                    <h2>${movie.Title}</h2>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDb Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="blank" class="btn btn-primary">View IMDb</a> 
+                    <a href="main.html" class="btn btn-default">Back to Search</a>
+                </div>
+            </div>
+        `;
+
+        $('#Movie').html(output);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
